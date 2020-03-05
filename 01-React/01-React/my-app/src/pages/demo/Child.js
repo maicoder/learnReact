1a13482437c3c035bed58df1bd042ae199cb1747
maicoder <hkdjcmc@msn.cn>
2020-03-05 16:03:10 +0800

    Update Child.js

--- 01-React/01-React/my-app/src/pages/demo/Child.js
+++ 01-React/01-React/my-app/src/pages/demo/Child.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
-export default class Life extends React.Component {
+export default class Child extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentWillMount(): void {
+    console.log('will Mount')
+  }
+
+  componentDidMount(): void {
+    console.log('did mount')
+  }
+
+  componentWillReceiveProps(nextProps: Readonly<P>, nextContext: any): void {
+    console.log('will props' + nextProps.name)
+  }
+
+  shouldComponentUpdate(nextProps: Readonly<P>, nextState: Readonly<S>, nextContext: any): boolean {
+    console.log('should update')
+    return true
+  }
+
+  componentWillUpdate(nextProps: Readonly<P>, nextState: Readonly<S>, nextContext: any): void {
+    console.log('will update')
+  }
+
+  componentDidUpdate(prevProps: Readonly<P>, prevState: Readonly<S>, snapshot: SS): void {
+    console.log('did update')
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+      </div>
+      )
   }
 }
