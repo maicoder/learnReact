b94b64e6f4293c59f4a186552268d14c176e349e
maicoder <hkdjcmc@msn.cn>
2020-05-19 01:32:16 +0800

    Update

--- 04-Bicycle/imoocmanager/src/admin.js
+++ 04-Bicycle/imoocmanager/src/admin.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Row, Col } from "antd";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-export default class Admin extends React.Components {
+export default class Admin extends React.Component {
 
+  render() {
+    return (
+      <div>
+        <Row className="container">
+          <Col span={3} className="nav-left">left</Col>
+          <Col span={21} className="main">
+            <Header></Header>
+            <Row>content</Row>
+            <Footer></Footer>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
