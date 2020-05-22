aa26f9c089a0de269a6183f80e27d7e45c76b2b9
maicoder <hkdjcmc@msn.cn>
2020-05-22 12:08:53 +0800

    Update index.js

---
+++ 04-Bicycle/imoocmanager/src/components/NavLeft/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Menu } from "antd";
+import menuConfig from "../../config/menuConfig";
+import "./index.less";
+
+const { SubMenu } = Menu;
+
+export default class NavLeft extends React.Component {
+  // componentWillMount() {
+  //   const menuTreeNode = this.renderMenu(menuConfig);
+  //   this.setState({menuTreeNode});
+  // }
+
+  UNSAFE_componentWillMount() {
+    const menuTreeNode = this.renderMenu(menuConfig);
+    this.setState({menuTreeNode});
+  }
+
+  // constructor(props) {
+  //   super(props);
+  //   const menuTreeNode = this.renderMenu(menuConfig);
+  //   this.setState({menuTreeNode});
+  // }
+
+  renderMenu = (data) => {
+    return data.map((item) => {
+      if (item.children) {
+        return (
+          <SubMenu title={item.title} key={item.key}>
+            { this.renderMenu(item.children) }
+          </SubMenu>
+        )
+      }
+      return <Menu.Item title={item.title} key={item.key}>{item.title}</Menu.Item>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+
+        <div className="logo">
+          <img src="/assets/logo-ant.svg" alt=""/>
+          <h1>MS</h1>
+        </div>
+
+        <Menu theme="dark" >
+          {this.state.menuTreeNode}
+        </Menu>
+
+      </div>
+    )
+  }
+}
