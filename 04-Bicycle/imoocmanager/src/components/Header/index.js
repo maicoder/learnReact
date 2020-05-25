495172cfe53c3bc1c26e23c21fded4ed54d917bf
maicoder <hkdjcmc@msn.cn>
2020-05-25 23:48:22 +0800

    Update index.js

--- 04-Bicycle/imoocmanager/src/components/Header/index.js
+++ 04-Bicycle/imoocmanager/src/components/Header/index.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { Row, Col } from "antd";
+import "./index.less";
+import Util from "../../utils/utils";
+import axios from "../../axios/";
 
 export default class Header extends React.Component {
+
+  state={}
+
+  UNSAFE_componentWillMount() {
+    this.setState(
+      {userName: "Hello,World"}
+    )
+
+    setInterval(() => {
+      let sysTime = Util.formateDate(new Date().getTime());
+      this.setState({
+        sysTime
+      })
+    }, 1000)
+  }
+
+  getWeatherAPIData() {
+    axios.jsonp({
+
+    }).then((res) => {
+
+    })
+  }
+
   render() {
     return (
-      <div>this is header</div>
+      <div className="header">
+        <Row className="header-top">
+          <Col span={24}>
+            <span>{this.state.userName}</span>
+            <a href="#">退出</a>
+          </Col>
+        </Row>
+        <Row className="breadcrumb">
+          <Col span={4} className="breadcrumb-title">首页</Col>
+          <Col span={20} className="weather">
+            <span className="date">{this.state.sysTime}</span>
+            <span className="weather-detail">晴</span>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
