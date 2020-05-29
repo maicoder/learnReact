01df0aa9b52c0f331086d427f37b07bc684c2a41
maicoder <hkdjcmc@msn.cn>
2020-05-29 19:58:41 +0800

    Create Topic.js

---
+++ 04-Bicycle/imoocmanager/src/react-router/Topic.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Topic extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Topic组件</h2>
+      </div>
+    );
+  }
+}
