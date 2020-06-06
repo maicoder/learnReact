6e0fba8968e85d9513cfa0ec455c54da0cc01723
maicoder <hkdjcmc@msn.cn>
2020-06-06 11:32:10 +0800

    Update index.js

--- 04-Bicycle/imoocmanager/src/components/Header/index.js
+++ 04-Bicycle/imoocmanager/src/components/Header/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Row, Col } from "antd";
 import "./index.less";
 import Util from "../../utils/utils";
-import axios from "../../axios/";
 
 export default class Header extends React.Component {
 
@@ -10,21 +9,13 @@ export default class Header extends React.Component {
     super(props);
     this.state = {
       userName: "Hello, React",
-      sysTime: null
+      sysTime: null,
+      weather: '',
+      weather_icon: '',
+      temperature: '',
+      week: '',
+      wind: ''
     }
-  }
-
-  UNSAFE_componentWillMount() {
-    // this.setState(
-    //   {userName: "Hello,World"}
-    // )
-
-    // setInterval(() => {
-    //   let sysTime = Util.formateDate(new Date().getTime());
-    //   this.setState({
-    //     sysTime
-    //   })
-    // }, 1000)
   }
 
   componentDidMount() {
@@ -32,6 +23,8 @@ export default class Header extends React.Component {
       () => this.tick(),
       1000
     );
+
+    this.getWeatherAPIData();
   }
 
   componentWillUnmount() {
@@ -46,10 +39,17 @@ export default class Header extends React.Component {
   }
 
   getWeatherAPIData() {
-    axios.jsonp({
-
-    }).then((res) => {
-
+    const url = "https://api.66mz8.com/api/weather.php?location=杭州"
+    fetch(url).then(res => {
+      res.json().then(resJson => {
+        this.setState({
+          weather: resJson.data[0].weather,
+          weather_icon: resJson.data[0].weather_icon,
+          week: resJson.data[0].week,
+          temperature: resJson.data[0].temperature,
+          wind: resJson.data[0].wind
+        })
+      })
     })
   }
 
@@ -66,7 +66,13 @@ export default class Header extends React.Component {
           <Col span={4} className="breadcrumb-title">首页</Col>
           <Col span={20} className="weather">
             <span className="date">{this.state.sysTime}</span>
-            <span className="weather-detail">晴</span>
+            <span className="week">{this.state.week}</span>
+            <span className="temperature">{this.state.temperature}</span>
+            <span className="weather-icon">
+              <img src={this.state.weather_icon} alt=""/>
+            </span>
+            <span className="weather-detail">{this.state.weather}</span>
+            <span className="wind">{this.state.wind}</span>
           </Col>
         </Row>
       </div>
