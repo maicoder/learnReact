2764b05512572d798baaaf80114201128e3cc5b2
maicoder <hkdjcmc@msn.cn>
2020-06-08 20:16:30 +0800

    Refactor CRA eject

---
+++ 04-Bicycle/imoocmanager/craco.config.js
@@ -0,0 +1,23 @@
+/* craco.config.js */
+
+const CracoLessPlugin = require('craco-less');
+const CracoAntDesignPlugin = require("craco-antd");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            // modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+    {
+      plugin: CracoAntDesignPlugin
+    }
+  ],
+};
