a3f6ee20a5dd1f1fc4ac6757c47c34752abbc222
maicoder <hkdjcmc@msn.cn>
2020-06-10 22:01:25 +0800

    Update index.js

--- 04-Bicycle/imoocmanager/src/pages/nomatch/index.js
+++ 04-Bicycle/imoocmanager/src/pages/nomatch/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class NoMatch extends Component {
   render() {
     return (
       <div>
-        <h2 style={{textAlign:"center"}}>404 No Found</h2>
+        <h2 style={{textAlign: "center"}}>404 No Found</h2>
       </div>
     );
   }
