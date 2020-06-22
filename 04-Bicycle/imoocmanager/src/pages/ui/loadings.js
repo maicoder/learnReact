225dfaa96e2c5eaee8cd0c86a3de42c35cae89da
maicoder <hkdjcmc@msn.cn>
2020-06-23 00:13:38 +0800

    Update loadings.js
    适配 Antd design 4

--- 04-Bicycle/imoocmanager/src/pages/ui/loadings.js
+++ 04-Bicycle/imoocmanager/src/pages/ui/loadings.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Card, Spin, Icon, Alert } from 'antd';
+import { LoadingOutlined } from "@ant-design/icons";
 import './index.less';
 
 class Loadings extends Component {
   render() {
-    const icon = <Icon type="loading" style={{fontSize:"24px"}}></Icon>
+    const icon = <LoadingOutlined style={{fontSize:"24px"}} spin />;
     return (
       <div>
         <Card title="Spin用法" className="Card">
@@ -15,7 +16,7 @@ class Loadings extends Component {
         </Card>
         <Card title="内容遮罩" className="Card">
           <Alert message="React" description="欢迎来到React实战课程" type="info"></Alert>
-          <Spin>
+          <Spin style={{marginTop: 10}}>
             <Alert message="React" description="欢迎来到React实战课程" type="warning"></Alert>
           </Spin>
           <Spin tip="加载中">
