0efa50e00b514f9415682a4993f21622413c3121
maicoder <hkdjcmc@msn.cn>
2020-06-23 00:12:21 +0800

    Update button.js
    适配 Antd design 4

--- 04-Bicycle/imoocmanager/src/pages/ui/button.js
+++ 04-Bicycle/imoocmanager/src/pages/ui/button.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
 import {Card, Button, Radio} from 'antd';
+import {
+  PlusOutlined,
+  EditOutlined,
+  DeleteOutlined,
+  SearchOutlined,
+  DownloadOutlined,
+  LeftOutlined,
+  RightOutlined
+} from "@ant-design/icons";
 import './index.less';
 
 class Buttons extends Component {
@@ -9,17 +18,19 @@ class Buttons extends Component {
         <Card title="基础按钮" className="Card">
           <Button type="primary">IMooc</Button>
           <Button>IMooc</Button>
-          <Button type="danger">IMooc</Button>
           <Button type="dashed">IMooc</Button>
+          <Button danger>IMooc</Button>
           <Button disabled>IMooc</Button>
+          <Button ghost>IMooc</Button>
+          <Button type="primary" style={{marginTop: 10}} block>IMooc</Button>
         </Card>
         <Card title="图形按钮" className="Card">
-          <Button icon="plus">创建</Button>
-          <Button icon="edit">编辑</Button>
-          <Button icon="delete">删除</Button>
-          <Button shape="circle" icon="search"></Button>
-          <Button type="primary" icon="search">搜索</Button>
-          <Button type="primary" icon="download">下载</Button>
+          <Button icon={<PlusOutlined />}>创建</Button>
+          <Button icon={<EditOutlined />}>编辑</Button>
+          <Button icon={<DeleteOutlined />}>删除</Button>
+          <Button shape="circle" icon={<SearchOutlined />}></Button>
+          <Button type="primary" icon={<SearchOutlined />}>搜索</Button>
+          <Button type="primary" icon={<DownloadOutlined />}>下载</Button>
         </Card>
         <Card title="loading按钮" className="Card">
           <Button type="primary" loading={true}>确定</Button>
@@ -30,15 +41,15 @@ class Buttons extends Component {
         </Card>
         <Card title="按钮组" className="Card">
           <Button.Group>
-            <Button type="primary" icon="left" style={{marginRight: "-2px"}}>返回</Button>
-            <Button type="primary" icon="right">前进</Button>
+            <Button type="primary" icon={<LeftOutlined />} style={{marginRight: "-2px"}}>返回</Button>
+            <Button type="primary" icon={<RightOutlined />}>前进</Button>
           </Button.Group>
         </Card>
         <Card title="按钮尺寸">
           <Button type="primary" size="large">IMooc</Button>
           <Button>IMooc</Button>
           <Button type="dashed" size="small">IMooc</Button>
-          <Button type="danger">IMooc</Button>
+          <Button type="primary" danger>IMooc</Button>
           <Radio.Group>
             <Radio value="small">小</Radio>
             <Radio value="default">中</Radio>
